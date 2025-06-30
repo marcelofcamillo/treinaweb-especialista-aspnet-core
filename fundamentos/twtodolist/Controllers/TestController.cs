7a01a7b3fc27690db413fa993acb94a747da6cbf
Marcelo Camillo <marcelocamillo18@gmail.com>
2025-06-30 15:02:02 -0300

    Enviando dados do Controller para a View com ViewData, ViewBag e TempData

--- fundamentos/twtodolist/Controllers/TestController.cs
+++ fundamentos/twtodolist/Controllers/TestController.cs
@@ -1,10 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using twtodolist.Models;
 
 namespace twtodolist.Controllers;
 
 public class TestController : Controller
 {
     public IActionResult Index()
+    {
+        var todo = new Todo
+        {
+            Title = "Estudar ASP .NET Core",
+            Description = "Concluir o curso de ASP .NET Core",
+        };
+
+        //ViewData["todo"] = todo;
+        ViewBag.todo = todo;
+
+        TempData["message"] = "Mensagem vinda da action Index";
+        return View();
+    }
+
+    public IActionResult Message()
     {
         return View();
     }
